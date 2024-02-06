d60ab07b1b4c97280b89e5c0f41ff52df4689dc9
Moon Choi <choimy1991@gmail.com>
2024-02-06 11:24:57 +0900

    Review 서블릿/JSP 강의 20 - 서블릿 필터(Servlet Filter)

--- workspace/JSPProj/src/main/java/com/newlecture/web/NoticeReg.java
+++ workspace/JSPProj/src/main/java/com/newlecture/web/NoticeReg.java
@@ -16,9 +16,10 @@ public class NoticeReg extends HttpServlet{
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		response.setCharacterEncoding("UTF-8"); //인코딩 방식 지정해줘야 한글 안깨짐. 쓰기해서 보내기. 
-		response.setContentType("text/html; charset=UTF-8"); //받을 때 이렇게 읽어라. text/html은 형식. 브라우저야 이대로 해석라.
+		response.setContentType("text/html; charset=UTF-8"); //받을 때 이렇게 읽어라. text/html은 형식. 브라우저야 이대로 해석해라.
 		// Response header에 Content-Type: text/html;charset=UTF-8로 지정됌. 브라우저가 자의적으로 컨텐트 해석하는 것을 방지함. 
 		
+		//이것 대신 서블릿 필터를 사용함 (CharacterEncodingFilter). 
 //		request.setCharacterEncoding("UTF-8"); //폼 안의 값이 한글일 때 submit 후 안깨지게끔. 
 		String title = request.getParameter("title"); //reg.html 에서 name 값을 받아옴. 
 		String content = request.getParameter("content");
--- workspace/JSPProj/src/main/java/com/newlecture/web/filter/CharacterEncodingFilter.java
+++ workspace/JSPProj/src/main/java/com/newlecture/web/filter/CharacterEncodingFilter.java
@@ -19,11 +19,21 @@ public class CharacterEncodingFilter implements Filter {
 		
 		System.out.println("before filter");
 		
-		request.setCharacterEncoding("UTF-8"); //모든 서블릿은 인코딩 필터 적용 후.
+		//서블릿이 실행돼기 전에 인코딩 완료시키기. 
+		request.setCharacterEncoding("UTF-8"); 
+		//모든 서블릿은 인코딩 필터가 적용됀 환경을 갖게됌. 
 		//^필터에 인코딩을 적용하고 서블릿은 이제 인코딩 개입 안함. 
-		chain.doFilter(request,  response); //다음 필터나 다음 서블렛이 실행됌.
 		
-		System.out.println("after filter");
+		chain.doFilter(request,  response); //요청이 오면 흐름을 넘겨서 다음 필터나 다음 서블렛이 실행됌.
+		
+		System.out.println("after filter"); //이 줄은 다음 서블릿이나 필터가 실행돼고 난 뒤 실행됌.
 	}
 
 }
+
+/*
+인코딩한 방법 request.setCharacterEncoding("UTF-8")
+	1. Noticereg.java에서 인코딩
+	2. 필터안에서 인코딩 CharacterEncodingFilter.java
+	3. 어노테이션으로 @WebFilter("/*"): web.xml에서 filter-mapping 불필요해짐.
+*/
